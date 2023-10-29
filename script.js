70b9e2fe57fe89ea3b1289dfd8450286f1ae9161
Bereket Tadesse <bekimehari@gmail.com>
2023-10-29 19:02:55 -0400

    update

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ function costCalculation() {
   let checkin = new Date(document.getElementById("checkindate").value);
   let checkout = new Date(document.getElementById("checkoutdate").value);
 
-  let daysDifference = (checkout - checkin) / (1000 * 3600 * 24) + 1;
-  document.getElementById("numberOfDays").value = daysDifference;
+  console.log(checkin);
+  if (checkin != "" && checkout != "") {
+    let daysDifference = (checkout - checkin) / (1000 * 3600 * 24) + 1;
+    document.getElementById("numberOfDays").value = daysDifference;
+  }
 
-  let numOfAdults = document.getElementById("dropdown").value;
+  if (checkin != "" && checkout != "") {
+    let numOfAdults = document.getElementById("dropdown").value;
 
-  let cost = numOfAdults * daysDifference * 150;
-
-  document.getElementById("cost").value = cost;
+    let cost = numOfAdults * daysDifference * 150;
+    document.getElementById("cost").value = cost;
+  }
 }
 
 setInterval(costCalculation, 1000);

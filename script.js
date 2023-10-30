78474972ed2dc731fb84105aee2b66748f71a18e
Bereket Tadesse <bekimehari@gmail.com>
2023-10-29 23:46:49 -0400

    update

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 function costCalculation() {
+  // checkin and checkout date values
   let checkin = new Date(document.getElementById("checkindate").value);
   let checkout = new Date(document.getElementById("checkoutdate").value);
 
-  console.log(checkin);
-  if (checkin != "" && checkout != "") {
+  //check if date has been entered
+  if (!isNaN(checkin) && !isNaN(checkout)) {
     let daysDifference = (checkout - checkin) / (1000 * 3600 * 24) + 1;
+
+    //set days value
     document.getElementById("numberOfDays").value = daysDifference;
-  }
 
-  if (checkin != "" && checkout != "") {
     let numOfAdults = document.getElementById("dropdown").value;
-
+    //find cost
     let cost = numOfAdults * daysDifference * 150;
-    document.getElementById("cost").value = cost;
+    // set cost value
+    document.getElementById("cost").value = cost + "$";
+  } else {
+    //clear
+    document.getElementById("numberOfDays").value = "";
+    document.getElementById("cost").value = "";
   }
+  console.log("hey");
 }
 
-setInterval(costCalculation, 1000);
+function clearElements() {
+  let form = document.getElementsByClassName("row");
+
+  let inputElements = form.querySelectorAll("input");
+
+  inputElements.forEach((element) => {
+    element.value = "";
+  });
+}
+
+resetButton = document.getElementById("Reset");
+resetButton.onClick = clearElements();
+//resetButton.addEventListener("click", clearElements);
+// call function every 0.5 second
+setInterval(costCalculation, 500);

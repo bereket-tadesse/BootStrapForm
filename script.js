95f7e8fc2c33f4f9965887e4850a1b77730e28b3
Bereket Tadesse <bekimehari@gmail.com>
2023-11-02 03:13:17 -0400

    update

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
-function costCalculation() {
-  // checkin and checkout date values
-  let checkinString = new Date(document.getElementById("checkindate").value);
-  let checkoutString = new Date(document.getElementById("checkoutdate").value);
+// wraper for js to make sure it loads after DOM is loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Your code here
 
-  let checkin = moment(checkinString);
-  let checkout = moment(checkoutString);
-
-  //check if date has been entered
-  if (!isNaN(checkin) && !isNaN(checkout)) {
-    //set days value
-    document.getElementById("numberOfDays").value = checkout.diff(
-      checkin,
-      "days"
+  function costCalculation() {
+    // checkin and checkout date values
+    console.log("working");
+    let checkinString = new Date(document.getElementById("checkindate").value);
+    let checkoutString = new Date(
+      document.getElementById("checkoutdate").value
     );
-    let numOfAdults = document.getElementById("dropdown").value;
 
-    // set cost value
-    document.getElementById("cost").value =
-      numOfAdults * checkout.diff(checkin, "days") * 150 + "$";
-  } else {
-    //clear
-    document.getElementById("numberOfDays").value = "";
-    document.getElementById("cost").value = "";
+    let checkin = moment(checkinString);
+    let checkout = moment(checkoutString);
+
+    //check if date has been entered
+    if (!isNaN(checkin) && !isNaN(checkout)) {
+      //set days value
+      document.getElementById("numberOfDays").value = checkout.diff(
+        checkin,
+        "days"
+      );
+      let numOfAdults = document.getElementById("dropdown").value;
+
+      // set cost value
+      document.getElementById("cost").value =
+        numOfAdults * checkout.diff(checkin, "days") * 150 + "$";
+    } else {
+      //clear
+      document.getElementById("numberOfDays").value = "";
+      document.getElementById("cost").value = "";
+    }
   }
-}
 
-function clearElements() {
-  let form = document.getElementsByClassName("row");
+  function clearElements() {
+    let form = document.getElementsByClassName("row");
 
-  let inputElements = form.querySelectorAll("input");
+    let inputElements = form.querySelectorAll("input");
 
-  inputElements.forEach((element) => {
-    element.value = "";
-  });
-}
+    inputElements.forEach((element) => {
+      element.value = "";
+    });
+  }
 
-function hey() {
-  console.log("hey");
-}
+  function hey() {
+    console.log("hey");
+  }
 
-document.getElementById("checkoutdate").addEventListener("click", hey);
+  document
+    .getElementById("checkindate")
+    .addEventListener("change", costCalculation());
+
+  document
+    .getElementById("checkoutdate")
+    .addEventListener("change", costCalculation());
+});

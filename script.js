53411f67675022a4e5ed08c5b9ce416ca7679c67
Bereket Tadesse <bekimehari@gmail.com>
2023-11-04 20:50:22 -0400

    update final

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
-// const testbutton = document.getElementById("b");
-
-// //testbutton.addEventListener(onclick, hey());
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   alert("document loaded!");
-//   testbutton.addEventListener("onclick", hey());
-// });
-
 function costCalculation() {
+  toastr.success("runnign");
   // checkin and checkout date values
-  console.log("working");
   let checkinString = new Date(document.getElementById("checkindate").value);
   let checkoutString = new Date(document.getElementById("checkoutdate").value);
-
+  //moment objects
   let checkin = moment(checkinString);
   let checkout = moment(checkoutString);
 
-  //check if date has been entered
+  //check if date has been entered and is +ve
   if (!isNaN(checkin) && !isNaN(checkout)) {
     //set days value
-    document.getElementById("numberOfDays").value = checkout.diff(
-      checkin,
-      "days"
-    );
-    let numOfAdults = document.getElementById("dropdown").value;
+    let dayDifference = checkout.diff(checkin, "days");
 
+    let costValue =
+      document.getElementById("dropdown").value * dayDifference * 150;
+
+    //set days
+    document.getElementById("numberOfDays").value = dayDifference;
     // set cost value
-    document.getElementById("cost").value =
-      numOfAdults * checkout.diff(checkin, "days") * 150 + "$";
+    document.getElementById("cost").value = costValue;
   } else {
     //clear
     document.getElementById("numberOfDays").value = "";
@@ -47,14 +38,78 @@ function clearElements() {
 
 document
   .getElementById("checkindate")
-  .addEventListener("change", costCalculation());
-
+  .addEventListener("change", costCalculation);
 document
   .getElementById("checkoutdate")
-  .addEventListener("change", costCalculation());
+  .addEventListener("change", costCalculation);
 
-const button = document.getElementById("shit");
-button.addEventListener("click", hey());
-function hey() {
-  console.log("hey");
+// parent ID of inputs
+let divIDs = [
+  "userNameDiv",
+  "firstNameDiv",
+  "lastNameDiv",
+  "phoneDiv",
+  "faxDiv",
+  "emailDiv",
+];
+
+//all input ids
+let inputElements = document.querySelectorAll("input[id]");
+
+//submit button ( using jquery)
+$("#submit").on("click", function () {
+  let testPassed = true;
+  let i = 0;
+  while (i <= 5) {
+    //input id
+    let id = `#${inputElements[i].id}`;
+    //Div classes
+    let classid = `#${divIDs[i]}`;
+
+    let inputValue = $(id).val();
+    if (!inputValue) {
+      toastr.error("Enter: " + id);
+      $(classid).addClass("has-error");
+      testPassed = false;
+    } else {
+      $(classid).removeClass("has-error");
+    }
+
+    i++;
+  }
+
+  if (!$("#cost").val()) {
+    toastr.error("Cost is not calculated!");
+  } else if ($("#cost").val() < 0) {
+    toastr.error("Cost is negative!");
+  } else if (testPassed) {
+    toastr.success("Sibmitted Succefully!");
+  }
+});
+
+// for removing the red error realtime
+let c = 0;
+while (c <= 5) {
+  //input id
+  let id = `#${inputElements[c].id}`;
+  //Div classes
+  let classid = `#${divIDs[c]}`;
+
+  $(id).on("input", function () {
+    let inputValue = $(id).val();
+    if (inputValue) {
+      $(classid).removeClass("has-error");
+    }
+  });
+  c++;
 }
+
+// reset button
+$("#reset").on("click", function () {
+  toastr.success("Cleard!");
+  inputElements.forEach((e) => {
+    e.value = "";
+  });
+  //removing textarea val
+  $("#message").val("");
+});
